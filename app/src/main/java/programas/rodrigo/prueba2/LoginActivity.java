19ffddc8d4c3812beaed8da0e4cd527fd04a6977
Rodrigo Muñoz <rodrigom2512@gmail.com>
2020-11-29 20:08:12 -0300

    Creación de Activity:Registro y Login

---
+++ app/src/main/java/programas/rodrigo/prueba2/LoginActivity.java
@@ -0,0 +1,14 @@
+package programas.rodrigo.prueba2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
